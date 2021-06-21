888442b4ab568423c194efd7a97b82563a6d6974
Danielle Smith <smith.ddms@gmail.com>
2021-06-21 11:32:00 -0500

    Completed Encapsulation Exercise

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+            Console.WriteLine("Please enter your name to begin.");
+            Name = Console.ReadLine();
+
+        }
+        
+        public string Name { get; set; }
+
+        private double balance = 0;
+
+        public void Deposit(double addMoney)
+        {
+            balance = balance + addMoney;
+            Console.WriteLine($"Thank you {Name}. You have made a deposit of {addMoney} to your account.");
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine($"{Name} Account Balance: {balance}");
+        }
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount newAccount = new BankAccount();
+            newAccount.Deposit(25);
+            newAccount.GetBalance();
+        }
+    }
+}
